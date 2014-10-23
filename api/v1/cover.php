97d135af6ccf40641e38fb6e06558762191ca1b1
Max Gurela <maxpowa1@gmail.com>
2014-10-22 22:55:59 -0700

    Clean up

--- api/v1/cover.php
+++ api/v1/cover.php
@@ -1,22 +1,18 @@
 <?php
+    ///////////////////////////////////////////////////////////////////////
+    // cover.php - fetches album art from given file (internal use only) //
+    ///////////////////////////////////////////////////////////////////////
+    
+    # Requires getid3() lib
 	require_once('../../getid3/getid3.php');
     
     if ( isset( $_GET[ 'file' ] ) ) {
         $filename = realpath(strip_tags( trim( $_GET[ 'file' ] ) ) );
     }
     
-    #echo '<pre>'.$filename.'</pre>';
-    #define('GETID3_HELPERAPPSDIR', 'D:/Polymer_Playground/getid3/helperapps');
-    
-    #if (file_exists($filename)) {
-    #    echo '<pre>'.'File exists'.'</pre>';
-    #}
-    
     $getID3 = new getID3;
     #$getID3->option_tag_id3v2 = true; # We don't /need/ to force tags to be id3v2
     $getID3->analyze($filename);
-    
-    #echo '<pre>'.htmlentities(print_r($getID3, true)).'</pre>';
     
     if (isset($getID3->info['id3v2']['APIC'][0]['data'])) {
         $cover = $getID3->info['id3v2']['APIC'][0]['data'];
@@ -34,8 +30,6 @@
         $mimetype = 'image/jpeg';
         // or null; depends on your needs 
     }
-
-    #$cover = null;
     
     if (!is_null($cover)) {
         // Send file 
