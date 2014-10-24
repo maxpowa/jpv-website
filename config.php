8a7e1904ccb6e3f2d9c9fca667f48770db8d4b97
Max Gurela <maxpowa1@gmail.com>
2014-10-23 19:26:36 -0700

    Strike that, reverse it.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     define('ROOT_DIR', dirname(__FILE__) . '/');
-    define('MEDIA_DIR', $ROOT_DIR . 'media/');
-    define('CACHE_DIR', $ROOT_DIR . 'cache/');
-    define('GETID3_DIR', $ROOT_DIR . 'getid3/');
-    define('LIB_DIR', $ROOT_DIR . 'lib/');
+    define('MEDIA_DIR', ROOT_DIR . 'media/');
+    define('CACHE_DIR', ROOT_DIR . 'cache/');
+    define('GETID3_DIR', ROOT_DIR . 'getid3/');
+    define('LIB_DIR', ROOT_DIR . 'lib/');
 ?>
