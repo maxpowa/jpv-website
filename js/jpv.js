3708ea69febbfcf08438d682cd08878ce0f9df7d
Lizbeth Rika <vazkii@hotmail.com>
2014-10-27 20:44:19 +0000

    BTSync stuff and some touches.

--- js/jpv.js
+++ js/jpv.js
@@ -25,14 +25,15 @@ $(document).on('click', '.song-play-button', function() {
 	var dl = $(this).parent('div').find('a').attr('href');	
 	var parent = $(this).closest('.song-box');
 	var title = parent.find('.song-title').text();
+	var player = $('#player');	
 	
 	var artist = parent.find('.song-artist').text();
 	var albumArtist = parent.find('.song-artist').attr('data-original-title');
 	var displayArtist = artist;
 	if(artist != albumArtist)
 		displayArtist = artist + " / " + albumArtist;
-
-	var audio = '<span class="player-song-name" data-toggle="tooltip" title="' + displayArtist + '">' + title + '</span><br><audio controls autoplay><source src="' + dl + '" type="audio/mpeg"></audio>';
+		
+	var audio = '<span class="player-song-name" data-toggle="tooltip" title="' + displayArtist + '">' + title + '</span><br><audio controls="controls" autoplay="autoplay"><source src="' + dl + '" type="audio/mpeg"></audio>';
 	
 	if(currentPlaying != undefined)
 		currentPlaying.animate({
@@ -44,23 +45,51 @@ $(document).on('click', '.song-play-button', function() {
 	});
 	currentPlaying = parent;
 	
-	var player = $('#player');	
 	player.html(audio);
 	if(player.css('display') == 'none')
 		player.show(600);
 	resetTooltip();
 });
 
+$('#btsync-key').click(function() {
+	$('#btsync-hide-button').show(600);
+	$('#btsync-hidden-key').show(600);
+	$('#btsync-hidden-qr').hide(600);
+});
+
+$('#btsync-installer').click(function() {
+	window.open('https://link.getsync.com/#f=JPV%20Media%20Folder&sz=35E8&s=P3MGCJYFIYDLHH5OTZ2IHUJK5HRXUHKS&i=CVSZIPOSYQ5MKBMPEZ5DKKRV3MVSG35VR&p=CDXRVCURLM4XKYRC4SJH4BFM7JJEMVEX', '_blank');
+});
+
+$('#btsync-qr').click(function() {
+	$('#btsync-hide-button').show(600);
+	$('#btsync-hidden-qr').show(600);
+	$('#btsync-hidden-key').hide(600);
+});
+
+$('#btsync-hide-button').click(function() {
+	$('#btsync-hide-button').hide(600);
+	$('#btsync-hidden-key').hide(600);
+	$('#btsync-hidden-qr').hide(600);
+});
+
 function selectAndSetContents(genre) {
 	getContents(genre);
-	$('.synced-color').animate({
+	$('.synced-bgcolor').animate({
 		'background-color': colors[genre],
+	});
+	$('.synced-color').animate({
+		'color': colors[genre],
 	});
 
 	$('html, body').animate({  scrollTop: 0 }, 500);
 	$('.nav li').removeClass('active');
 	$('.nav li[genre=' +  genre + ']').addClass('active');
 	document.title = $('.nav li[genre=' +  genre + ']').attr('title') + ' | JPV Playlist';
+	
+	if(genre == 'all')
+		$('#content-info').show(600);
+	else $('#content-info').hide(600);
 }
 
 function getContents(genre) {
