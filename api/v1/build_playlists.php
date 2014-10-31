f4d4806fbdf3859b32421031250611c6f82e6085
Max Gurela <maxpowa1@gmail.com>
2014-10-30 19:06:49 -0700

    >.<

--- api/v1/build_playlists.php
+++ api/v1/build_playlists.php
@@ -25,7 +25,7 @@
 			else $current_list = iterate_dir(MEDIA_DIR . $file, $current_list, $exclusions);
 		}
         $existing = file_get_contents(MEDIA_DIR . $name);
-        if strcmp($existing, $current_list) == 0 {
+        if (strcmp($existing, $current_list) == 0) {
             return;
         }
 		file_put_contents(MEDIA_DIR . $name, $current_list);
